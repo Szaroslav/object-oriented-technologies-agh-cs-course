e97c6a0ff566018d5fe26498ad95985177d02713
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-10-09 18:15:35 +0200

    Finish StudentDAO implementation of basic methods

--- lab1/lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/dao/StudentDao.java
+++ lab1/lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/dao/StudentDao.java
@@ -4,19 +4,36 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
+import org.hibernate.Session;
 import pl.edu.agh.iisg.to.model.Course;
 import pl.edu.agh.iisg.to.model.Student;
+
+import javax.persistence.PersistenceException;
 
 public class StudentDao extends GenericDao<Student> {
 
     public Optional<Student> create(final String firstName, final String lastName, final int indexNumber) {
-        //TODO - implement
-        return Optional.empty();
+        Student student = new Student(firstName, lastName, indexNumber);
+        try {
+            save(student);
+            return Optional.of(student);
+        }
+        catch (PersistenceException e) {
+            return Optional.empty();
+        }
     }
 
     public Optional<Student> findByIndexNumber(final int indexNumber) {
-        //TODO - implement
-        return Optional.empty();
+        try (Session session = currentSession()) {
+            Student student = (Student) session
+                .createQuery("SELECT s FROM Student s WHERE s.indexNumber = :indexNumber")
+                .setParameter("indexNumber", indexNumber)
+                .getSingleResult();
+            return Optional.of(student);
+        }
+        catch (PersistenceException e) {
+            return Optional.empty();
+        }
     }
 
     public Map<Course, Float> createReport(final Student student) {
