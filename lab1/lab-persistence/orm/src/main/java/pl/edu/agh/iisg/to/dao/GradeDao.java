2fd826dde755614c4617f3599d484b02404f52c8
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-10-09 23:25:04 +0200

    Finish GradeDAO task

--- lab1/lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/dao/GradeDao.java
+++ lab1/lab-persistence/orm/src/main/java/pl/edu/agh/iisg/to/dao/GradeDao.java
@@ -4,12 +4,25 @@ import pl.edu.agh.iisg.to.model.Course;
 import pl.edu.agh.iisg.to.model.Grade;
 import pl.edu.agh.iisg.to.model.Student;
 
+import javax.persistence.PersistenceException;
+
 public class GradeDao extends GenericDao<Grade> {
 
     public boolean gradeStudent(final Student student, final Course course, final float grade) {
-        //TODO implement
-        return true;
-    }
+        try {
+            Grade g = new Grade(student, course, grade);
+            if (student.gradeSet().contains(g) || course.gradeSet().contains(g))
+                return false;
 
+            student.gradeSet().add(g);
+            course.gradeSet().add(g);
+            save(g);
+
+            return true;
+        }
+        catch (PersistenceException e) {
+            return false;
+        }
+    }
 
 }
