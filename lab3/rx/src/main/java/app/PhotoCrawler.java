437400de918e0b380f2dcdbd14404f29cd2c22b4
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-10-23 23:16:13 +0200

    [Laboratory 3] Init laboratory, finish task 1 and 2

---
+++ lab3/rx/src/main/java/app/PhotoCrawler.java
@@ -0,0 +1,50 @@
+package app;
+
+import io.reactivex.rxjava3.disposables.Disposable;
+
+import util.PhotoDownloader;
+import util.PhotoProcessor;
+import util.PhotoSerializer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PhotoCrawler {
+
+    private static final Logger log = Logger.getLogger(PhotoCrawler.class.getName());
+
+    private final PhotoDownloader photoDownloader;
+
+    private final PhotoSerializer photoSerializer;
+
+    private final PhotoProcessor photoProcessor;
+
+    public PhotoCrawler() throws IOException {
+        this.photoDownloader = new PhotoDownloader();
+        this.photoSerializer = new PhotoSerializer("./photos");
+        this.photoProcessor = new PhotoProcessor();
+    }
+
+    public void resetLibrary() throws IOException {
+        photoSerializer.deleteLibraryContents();
+    }
+
+    public void downloadPhotoExamples() {
+        try {
+            Disposable disposable = photoDownloader.getPhotoExamples()
+                .subscribe(photoSerializer::savePhoto);
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Downloading photo examples error", e);
+        }
+    }
+
+    public void downloadPhotosForQuery(String query) throws IOException {
+        // TODO Implement me :(
+    }
+
+    public void downloadPhotosForMultipleQueries(List<String> queries) {
+        // TODO Implement me :(
+    }
+}
