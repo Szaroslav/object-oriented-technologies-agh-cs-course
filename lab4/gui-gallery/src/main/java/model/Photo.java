a1dee0d1169f8746b9f76125d6b39c456c08e323
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-10-24 17:23:50 +0200

    [Laboratory 4] Finish model task

--- lab4/gui-gallery/src/main/java/model/Photo.java
+++ lab4/gui-gallery/src/main/java/model/Photo.java
@@ -12,24 +12,32 @@ import javafx.scene.image.Image;
 
 public class Photo {
 
-    private String name;
+    private StringProperty name;
 
-    private final Image photoData;
+    private final ObjectProperty<Image> photoData;
 
     public Photo(String extension, byte[] photoData) {
-        this.photoData = new Image(new ByteArrayInputStream(photoData));
-        this.name = UUID.randomUUID().toString() + "." + extension;
+        this.photoData = new SimpleObjectProperty<>(new Image(new ByteArrayInputStream(photoData)));
+        this.name = new SimpleStringProperty(UUID.randomUUID().toString() + "." + extension);
     }
 
     public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public Image getPhotoData() {
+        return photoData.get();
+    }
+
+    public ObjectProperty<Image> photoDataProperty() {
         return photoData;
     }
 }
